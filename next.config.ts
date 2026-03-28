2b1e4ad83054c2ddd74a70ca3c4aed3667109158
Arya <navahcayra@gmail.com>
2026-03-28 18:03:43 +0530

    bug fix 1.1

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,17 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/legacy",
-        destination: "https://landing-red-chi.vercel.app/",
+        source: "/legacy/:path*",
+        destination: "https://landing-red-chi.vercel.app/:path*",
       },
-    {
-      source: "/FSD/:path",
-      destination: "https://ryachavan.github.io/FSD/:path",
-    },
+      {
+        source: "/FSD/:path*",
+        destination: "https://ryachavan.github.io/FSD/:path*",
+      },
+      {
+        source: "/FSD/:path",
+        destination: "https://ryachavan.github.io/FSD/:path",
+      },
     ];
   },
 };
